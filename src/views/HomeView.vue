a5a1684d4f66747437096ad6cb029dc496853f7b
Brian Hasal <brian.hasal@gmail.com>
2022-08-07 06:34:55 -0700

    frontend talking to backend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,44 @@
+<script>
+import axios from 'axios';
+
+  export default {
+    data: function () {
+      return {
+        apis: []
+
+      };
+    },
+    created: function () {
+      this.indexApis();
+    },
+    methods: {
+      indexApis: function () {
+        axios.get("/api.json").then((response) => {
+          console.log("Retrieving APIs", response);
+          this.apis = response.data;
+        })
+      }
+    
+    },
+  };
+</script>
+
+
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>LIST OF APIS</h1>
+    <p>{{this.apis}}</p>
+    <div v-for="api in apis" :key="api.id">
+      <h3>
+        {{api.stars}} Stars
+        {{api.watchers}} Watchers
+        {{api.forks}} Forks
+      </h3>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+
+<style></style>
