f588b24bbc62a14f461a267734ade6624078b6c3
Brian Hasal <brian.hasal@gmail.com>
2022-08-15 07:13:39 -0700

    Added OAuth for my Account

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,29 @@
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      profile: {},
+    };
+  },
+  created: function () {
+    var githubAccessToken = localStorage.getItem("github_access_token");
+    if (githubAccessToken) {
+      console.log(githubAccessToken);
+      axios.get("/github_profile?github_access_token=" + githubAccessToken).then((response) => {
+        console.log(response.data);
+        this.profile = response.data;
+      });
+    }
+  },
+};
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>GitHub profile!</h1>
+    <h2>{{ profile.name }}</h2>
+    <img v-bind:src="profile.avatar_url" alt="" />
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,44 +1,72 @@
 <script>
-import axios from 'axios';
+import axios from "axios";
 
-  export default {
-    data: function () {
-      return {
-        apis: []
+export default {
+  data: function () {
+    return {
+      apis: [],
+    };
+  },
+  created: function () {
+    var code = this.$route.query.code;
+    if (code) {
+      axios.get("/auth/github/callback?code=" + code).then((response) => {
+        console.log(response.data);
+        localStorage.setItem("github_access_token", response.data.access_token);
+        this.$router.push("/about");
+      });
+    }
 
-      };
+// http://localhost:3000/auth/github/callback?code=6fa712b2514f95dbdc31
+
+    this.indexApis();
+  },
+  methods: {
+    indexApis: function () {
+
+
+      // axios
+      //   .all([
+      //     axios.get("https://api.github.com/vuejs/vue"), 
+      //     axios.get("https://github.com/angular/angular.js"),
+      //     axios.get("https://github.com/emberjs/ember.js"),
+      //     axios.get("https://github.com/sveltejs/svelte"),
+      //     axios.get("https://github.com/facebook/react")
+      //     ])
+      //   .then(
+      //     axios.spread(function (response1, response2, response3, response4, response5) {
+      //       this.apis << response1,
+      //       this.apis << response2,
+      //       this.apis << response3,
+      //       this.apis << response4,
+      //       this.apis << response5
+      //     })
+      //   );
+
+      axios.get("/api").then((response) => {
+        console.log("Retrieving APIs", response);
+        this.apis = response.data;
+      })
     },
-    created: function () {
-      this.indexApis();
-    },
-    methods: {
-      indexApis: function () {
-        axios.get("/api.json").then((response) => {
-          console.log("Retrieving APIs", response);
-          this.apis = response.data;
-        })
-      }
-    
-    },
-  };
+  },
+};
 </script>
-
-
 
 <template>
   <div class="home">
     <h1>LIST OF APIS</h1>
-    <p>{{this.apis}}</p>
-    <div v-for="api in apis" :key="api.id">
+    <a href="https://github.com/login/oauth/authorize?client_id=0b609ab45aba0e6acc84">
+      Log in with Github ID
+    </a>
+    <p>{{ this.apis }}</p>
+    <!-- <div v-for="api in apis" :key="api.id">
       <h3>
         {{api.stars}} Stars
         {{api.watchers}} Watchers
         {{api.forks}} Forks
       </h3>
-    </div>
+    </div> -->
   </div>
 </template>
-
-
 
 <style></style>
